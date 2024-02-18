d3177bc8c1c58a6977bb45575b7e0f8ca21e6c82
Alex Salazar <19998979+alxanderapollo@users.noreply.github.com>
2024-02-18 15:07:57 -0500

    added some fetch requests to the DB with api endpoints

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,5 +1,5 @@
 # our table
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
 # depends is dependency injection, we need to do something before we excute what we execute
 # import our engine and our models
 import models
@@ -7,6 +7,7 @@ from models import Todos
 from database import engine, SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
+from starlette import status
 
 app = FastAPI()
 # creates everything from our models file, and Db file to create a new DB with Todos and all of the columns that have been layed out
@@ -31,6 +32,13 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 # annotated is from typing, session
 # return db.query takes our models of todos, and returns all of the items inside 
-@app.get("/")
+@app.get("/", status_code = status.HTTP_200_OK)
 async def read_all(db:db_dependency):
     return db.query(Todos).all()
+
+# return a match as soon as we see one
+@app.get('/todo/{todo_id}', status_code = status.HTTP_200_OK)
+async def read_todo(db: db_dependency, todo_id: int = Path(gt = 0) ):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None: return todo_model
+    raise HTTPException(status_code = 404, detail='Todo not found')
