9cf37f3ae56ad57aab56a5add0fcebd712eb1c6a
Alex Salazar <19998979+alxanderapollo@users.noreply.github.com>
2024-02-18 09:44:23 -0500

    Added HTTP status codes

--- books2.py
+++ books2.py
@@ -1,5 +1,8 @@
-from fastapi import Body, FastAPI
+# path is a way for us to validate path params
+from fastapi import FastAPI, Path,Query, HTTPException 
 from pydantic import BaseModel, Field
+# automatically installed with FASTAPI
+from starlette import status
 # to use optional and not required feilds need to be brough over through typing instead of 
 # pydantic
 from typing import Optional
@@ -13,13 +16,15 @@ class Book:
     author: str
     description:str
     rating: int
+    publishedDate: int
 
-    def __init__(self, id, title, author, description, rating):
+    def __init__(self, id, title, author, description, rating, publishedDate):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.publishedDate = publishedDate
 
 # this class serves as our validator for our book items
 class BookRequest(BaseModel):
@@ -28,7 +33,8 @@ class BookRequest(BaseModel):
     author: str =  Field(min_length=1)
     description: str =  Field(min_length=1, max_length=100)
     # gt means greater than so we're saying > than -1, lt means lessthan
-    rating:int =  Field(gt=0,lt=6)
+    rating:int = Field(gt=0,lt=6)
+    publishedDate:int = Field(gt=0, lt=2025)
 
     # config for having an automatic example in our schema
     class Config:
@@ -37,7 +43,8 @@ class BookRequest(BaseModel):
                 'title': 'A new book',
                 'author': 'codingwithroby',
                 'description': ' A new description of a book',
-                'rating': 5
+                'rating': 5,
+                'publishedDate': 2002
             }
         }
 
@@ -45,32 +52,49 @@ class BookRequest(BaseModel):
 # book request objt, to validate our book request so that we can transform it into  abook and add it to our list
 
 BOOKS = [ 
-    Book(1,'Blood Meridian', 'Cormac McCarthy', 'old west tale', 5),
-    Book(2,'All the pretty horses', 'Cormac McCarthy', 'romance', 5),
-    Book(3,'No Country for old men', 'Cormac McCarthy', 'western', 5),
-    Book(4,'Computer Science pro', 'coding with roby', 'comp sci book', 5),
-    Book(5,'physics', 'anthony pacheco', 'science book', 1),
-    Book(6,'principles of langauges', 'Tat Young Kong', 'College Textbook', 1),
-    Book(7,'Animal Farm', 'George Orwell', 'narrtive fiction in communisum', 3),
-    Book(8,'A heart so white', 'Javier marias', 'fiction', 2),
+    Book(1,'Blood Meridian', 'Cormac McCarthy', 'old west tale', 5, 2001),
+    Book(2,'All the pretty horses', 'Cormac McCarthy', 'romance', 5, 2001),
+    Book(3,'No Country for old men', 'Cormac McCarthy', 'western', 5, 2001),
+    Book(4,'Computer Science pro', 'coding with roby', 'comp sci book', 5, 2011),
+    Book(5,'physics', 'anthony pacheco', 'science book', 1, 2012),
+    Book(6,'principles of langauges', 'Tat Young Kong', 'College Textbook', 1, 2022),
+    Book(7,'Animal Farm', 'George Orwell', 'narrtive fiction in communisum', 3, 2024),
+    Book(8,'A heart so white', 'Javier marias', 'fiction', 2, 2001),
 ]
-@app.get("/books")
-async def readAllBooks():
+
+# status_code=status.HTTP_200_OK -> this means to send a status ok after returning what was asked for
+@app.get("/books", status_code=status.HTTP_200_OK)
+async def read_All_Books():
     return BOOKS
 
 
 # api endpoint to find a book based on an ID
-@app.get('/books/{book_id}')
-async def read_book(book_id:int):
+# our book id path param needs to be greater than 0 or it will throw an error
+@app.get('/books/{book_id}', status_code=status.HTTP_200_OK)
+async def read_book(book_id:int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
+        # if the book is not found return 404
+    
+    raise HTTPException(status_code=404, detail='Item not found')
         
-@app.get ('/books/')
-async def read_book_by_rating(book_rating: int):
+@app.get('/books/',status_code=status.HTTP_200_OK)
+async def read_book_by_rating(book_rating: int = Query(gt=0, lt = 6)):
     booksToReturn = []
     for book in BOOKS:
         if book.rating == book_rating:
+            booksToReturn.append(book)
+    
+    return booksToReturn
+
+
+# get books by publishing date
+@app.get('/books/published/',status_code=status.HTTP_200_OK)
+async def read_book_by_published_date(book_published: int = Query(gt=0, lt=2025)):
+    booksToReturn = []
+    for book in BOOKS:
+        if book.publishedDate == book_published:
             booksToReturn.append(book)
     
     return booksToReturn
@@ -84,10 +108,10 @@ async def read_book_by_rating(book_rating: int):
 3. Finally we just append the book to our list of books
 
 '''
-@app.post('/create-book')
+@app.post('/create-book',status_code=status.HTTP_201_CREATED)
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
-    BOOKS.append(findBookId(book_request))
+    BOOKS.append(findBookId(new_book))
 
 
 def findBookId(book:Book):
@@ -102,3 +126,39 @@ def findBookId(book:Book):
         book.id = 1
     
     return book
+
+# this is to update one of our objects, and to do it by ID
+
+# update objects
+@app.put('/books/update_book',status_code=status.HTTP_204_NO_CONTENT)
+async def update_book(book: BookRequest):
+    book_change = False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+            # if we found the book and change it set it True
+            book_change = True
+
+    if not book_change:
+        raise HTTPException(status_code=404, detail='Item not found')
+
+
+# delete objects
+
+@app.delete('/books/{book_id}',status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int = Path(gt=0)):
+    book_change = False
+
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            book_change = True
+            break
+    
+    if not book_change:
+        raise HTTPException(status_code=404, detail='Item not found')
+
+
+
+
+
