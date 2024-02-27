30ed45ec771978bcc649dc314f656323194d9bf3
Alex Salazar <19998979+alxanderapollo@users.noreply.github.com>
2024-02-27 18:18:09 -0500

    users can now change and update there passwords, and find all the information there is to know about themselves

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,7 @@
 # our table
 from fastapi import FastAPI
 from database import engine
-from routers import auth, todos,admin
+from routers import auth, todos,admin,users
 import models
 
 app = FastAPI()
@@ -13,3 +13,5 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
+app.include_router(users.router)
+
--- TodoApp/routers/users.py
+++ TodoApp/routers/users.py
@@ -1,14 +1,17 @@
 from models import Users
 from fastapi import APIRouter, Depends, HTTPException, Path
-from pydantic import Field
+from pydantic import Field, BaseModel
 from database import SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
 from starlette import status
 from .auth import get_current_user
+from passlib.context import CryptContext
+
+bcrypt_context = CryptContext(schemes=['bcrypt'],deprecated ='auto')
+
 
 router = APIRouter(
-     prefix='/users',
     tags=['users']
 )
 
@@ -24,15 +27,39 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
 
+# for verifying our pass
+class UserVerification(BaseModel):
+    password: str
+    new_password: str = Field(min_length=6)
+
 
 # get user
-@router.get('/user/{user_id}', status_code=status.HTTP_200_OK)
-async def get_user(user:user_dependency, db:db_dependency):
-    pass
+@router.get('/user/{email}', status_code=status.HTTP_200_OK)
+async def get_user(user:user_dependency, db:db_dependency, userEmail:str ):
+    if user is None: raise HTTPException(status_code=401, detail='Authentication Failed')
+
+    user_model = db.query(Users).filter(Users.email == userEmail).first()
+    if user_model is not None: return user_model
+    
+    raise HTTPException(status_code = 404, detail='User not found')
+
+@router.put('/user/{email}', status_code=status.HTTP_200_OK)
+async def update_password(user:user_dependency, db:db_dependency, user_verification: UserVerification):
+    if user is None: raise HTTPException(status_code=401, detail='Authentication Failed')
+
+    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+
+    # if the passwords dont match, raise an exception
+    if not bcrypt_context.verify(user_verification.password,user_model.hashed_password):
+        raise HTTPException(status_code=401, detail='Error on password Change')
+    # otherwise add the new pass as an updated pass to our user - and then add that user
+    user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
+    db.add(user_model)
+    db.commit()
 
 
 
-# change password
-@router.get('/user/{user_id}', status_code=status.HTTP_200_OK)
-async def get_user(user:user_dependency, db:db_dependency):
-    pass
+
+
+
+    
